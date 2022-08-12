0edd81c3a3cd2b35f74a8aa760a7ead25a704063
businesskaniki <businesskanikii@gmail.com>
2022-08-12 18:44:45 +0300

    using hooks

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,52 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
 import calculate from '../logic/calculate';
 
-export default class Calculator extends React.PureComponent {
-  constructor(props) {
-    super(props);
-    this.state = {
-      total: null,
-      next: null,
-      operation: null,
-    };
-  }
+const Calculator = () => {
+  const [initState, setInitState] = useState({
+    total: 0,
+    operation: null,
+    next: null,
+  });
 
-  clickListener = (e) => {
-    e.preventDefault();
+  const { total, operation, next } = initState;
+
+  const clickfunc = (e) => {
     const result = e.target.value;
-    const obj = calculate(this.state, result);
-    this.setState(obj);
+    setInitState((prevState) => calculate(prevState, result));
   };
 
-  render() {
-    // eslint-disable-next-line no-unused-vars
-    const { total, next, operation } = this.state;
-    const result = `${total || ''}${operation || ''}${next || ''}`;
-    return (
-      <form>
-        <div className="resultsDiv">{result}</div>
-        <div className="btnDiv">
-          <input type="button" value="AC" onClick={this.clickListener} />
-          <input type="button" value="+/-" onClick={this.clickListener} />
-          <input type="button" value="%" onClick={this.clickListener} />
-          <input className="orange" type="button" value="÷" onClick={this.clickListener} />
-          <input type="button" value="7" onClick={this.clickListener} />
-          <input type="button" value="8" onClick={this.clickListener} />
-          <input type="button" value="9" onClick={this.clickListener} />
-          <input className="orange" type="button" value="x" onClick={this.clickListener} />
-          <input type="button" value="4" onClick={this.clickListener} />
-          <input type="button" value="5" onClick={this.clickListener} />
-          <input type="button" value="6" onClick={this.clickListener} />
-          <input className="orange" type="button" value="-" onClick={this.clickListener} />
-          <input type="button" value="1" onClick={this.clickListener} />
-          <input type="button" value="2" onClick={this.clickListener} />
-          <input type="button" value="3" onClick={this.clickListener} />
-          <input className="orange" type="button" value="+" onClick={this.clickListener} />
-          <input className="span" type="button" value="0" onClick={this.clickListener} />
-          <input type="button" value="." onClick={this.clickListener} />
-          <input className="orange" type="button" value="=" onClick={this.clickListener} />
-        </div>
-      </form>
-    );
-  }
-}
+  const result = `${total || ''}${operation || ''}${next || ''}`;
+  return (
+    <form>
+      <div className="resultsDiv">{result}</div>
+      <div className="btnDiv">
+        <input type="button" value="AC" onClick={clickfunc} />
+        <input type="button" value="+/-" onClick={clickfunc} />
+        <input type="button" value="%" onClick={clickfunc} />
+        <input className="orange" type="button" value="÷" onClick={clickfunc} />
+        <input type="button" value="7" onClick={clickfunc} />
+        <input type="button" value="8" onClick={clickfunc} />
+        <input type="button" value="9" onClick={clickfunc} />
+        <input className="orange" type="button" value="x" onClick={clickfunc} />
+        <input type="button" value="4" onClick={clickfunc} />
+        <input type="button" value="5" onClick={clickfunc} />
+        <input type="button" value="6" onClick={clickfunc} />
+        <input className="orange" type="button" value="-" onClick={clickfunc} />
+        <input type="button" value="1" onClick={clickfunc} />
+        <input type="button" value="2" onClick={clickfunc} />
+        <input type="button" value="3" onClick={clickfunc} />
+        <input className="orange" type="button" value="+" onClick={clickfunc} />
+        <input className="span" type="button" value="0" onClick={clickfunc} />
+        <input type="button" value="." onClick={clickfunc} />
+        <input className="orange" type="button" value="=" onClick={clickfunc} />
+      </div>
+    </form>
+  );
+};
+
+export default Calculator;
